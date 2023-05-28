6f440081ec8eaa0f137c86c807a1aa6751cecf0c
Ashutosh Debnath <debnathashu752@gmail.com>
2023-05-29 01:16:11 +0600

    init 29/05/2023

---
+++ ipaywallet.ocmod/upload/admin/language/en-gb/extension/payment/ipay_wallet.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']      = 'iPay Bangladesh: An e-wallet &amp; secure payment platform';
+$_['text_ipay_wallet']   = '<a href="https://ipay.com.bd" target="_blank"><img src="view/image/payment/ipay.png" alt="iPay Bangladesh: An e-wallet &amp; secure payment platform" title="iPay - Developed By LockersIT" /></a>';
+
+// Text
+$_['text_extension']     = 'Extensions';
+$_['text_success']       = 'Success: You have modified ipay wallet details!';
+$_['text_edit']          = 'Edit iPay Wallet';
+
+// Entry
+$_['entry_ipay']         = 'iPay Transfer Instructions';
+$_['instraction_ipay']   = '01. Go to your iPay Wallet Menu.
+02. Choose “Send Money”.
+03. Enter the iPay Wallet Number you want to send money to.
+04. Enter the amount you want to send.
+05. Enter a note about the transaction.
+06. Now enter your iPay Wallet PIN to confirm the transaction.
+
+Done! You and the Receiver both will receive a confirmation message from ipay.';
+$_['entry_total']        = 'Total';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Help
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.'; 
+
+// Error 
+$_['error_permission']   = 'Warning: You do not have permission to modify payment ipay wallet!';
+$_['error_ipay']         = 'iPay Wallet Instructions Required!';
