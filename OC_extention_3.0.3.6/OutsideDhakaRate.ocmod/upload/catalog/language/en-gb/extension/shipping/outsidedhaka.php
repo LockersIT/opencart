e64fe865db0732d629888e01b497c90575b727e3
Ashutosh Debnath <debnathashu752@gmail.com>
2023-05-29 01:22:39 +0600

    init 29/05/2023

---
+++ OC_extention_3.0.3.6/OutsideDhakaRate.ocmod/upload/catalog/language/en-gb/extension/shipping/outsidedhaka.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_title']       = 'Outside Dhaka Rate';
+$_['text_description'] = 'Outside Dhaka Shipping Rate';
---
+++ OC_extention_3.0.3.6/rocket.ocmod/upload/catalog/language/en-gb/extension/payment/rocket_wallet.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title']       = '<img src="../admin/view/image/payment/rocket.png" alt="Rocket | Dutch-Bangla Bank Mobile Banking" title="Rocket | Dutch-Bangla Bank Mobile Banking" />';
+$_['text_instruction'] = 'Rocket Wallet Instructions';
+$_['text_description'] = 'Please transfer the total amount to the following Rocket account.';
+$_['text_payment']     = 'Your order will not ship until we receive payment.';
